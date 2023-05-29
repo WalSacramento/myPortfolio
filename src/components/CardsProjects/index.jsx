a6f05bb6d807c01e9cdffd2f223d8eaf34106a24
Waldsson Sacramento <waldsson.dev@gmail.com>
2023-05-29 17:34:33 -0300

    Adição de projetos

--- src/components/CardsProjects/index.jsx
+++ src/components/CardsProjects/index.jsx
@@ -13,21 +13,29 @@ export default function CardsProjects({
     <>
       <Card className={S.cardProjects} style={{ width: '18rem' }}>
         <Card.Img variant="top" src={imgSrc} />
-        <Card.Body>
+        <Card.Body className={S.cardBody}>
           <Card.Title className={S.title}>{title}</Card.Title>
           <Card.Text className={S.text}>{description}</Card.Text>
 
           <div className={S.buttons}>
-            <Button href={deploySrc} variant="outline" className={S.btnDeploy}>
-              Deploy
-            </Button>
-            <Button
-              href={repoSrc}
-              variant="primary"
-              className={S.btnRepository}
-            >
-              Repositório
-            </Button>
+            {deploySrc && (
+              <Button
+                href={deploySrc}
+                variant="outline"
+                className={S.btnDeploy}
+              >
+                Deploy
+              </Button>
+            )}
+            {repoSrc && (
+              <Button
+                href={repoSrc}
+                variant="primary"
+                className={S.btnRepository}
+              >
+                Repositório
+              </Button>
+            )}
           </div>
         </Card.Body>
       </Card>
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -4,9 +4,24 @@ import S from './styles.module.scss'
 import reactQuizImg from '../../img/quizReactImg.png'
 import alfaImportedImg from '../../img/alfaImportedImg.png'
 import topekaBankImg from '../../img/topekaBankImg.png'
+import devBooksImg from '../../img/devBooksImg.png'
+import apiCrudImg from '../../img/apiCRUDImg.png'
 
 export default function Projects() {
   const projectsData = [
+    {
+      title: 'DevBooks',
+      description: 'Nesse projeto, utilizei várias tecnologias e ferramentas como: Typescript, React, Styled Components, Axios. Um dos destaques do projeto é o consumo de dados da API do Google Books.',
+      imgSrc: apiCrudImg,
+      repoSrc: 'https://github.com/WalSacramento/APIREST_Prisma_e_Nodejs'
+    },
+    {
+      title: 'ApiREST',
+      description: 'Através dessa API, é possível a execução de operações CRUD (Create, Read, Update, Delete) em recursos específicos. Nesse projeto utilizei as tecnologias: Node.js, Express, Prisma e SQLite.',
+      imgSrc: devBooksImg,
+      deploySrc: 'https://devbooks-tau.vercel.app/',
+      repoSrc: 'https://github.com/WalSacramento/devbooks'
+    },
     {
       title: 'React Quiz',
       description:
@@ -20,7 +35,6 @@ export default function Projects() {
       description:
         'Este projeto foi desenvolvido para obtenção de nota na disciplina de "Programação Web 1". O objetivo do projeto é simular uma plataforma bancária online.',
       imgSrc: topekaBankImg,
-      deploySrc: ' ',
       repoSrc: 'https://github.com/camillaalves12/topeca_bank_React'
     },
     {
